b6387954420d5bd17de7eee65589d31aa6663b75
MariaDominguez8 <maria.e.dominguez.97@gmail.com>
2021-05-28 15:51:40 -0300

    cambios

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express(); 
 const port= 3000;
-app.listen(port, ()=> console.log("Servidor corriendo en el puerto"+ port))
+
+//req-->> request (petición)
+//res -->> response (respuesta)
+
+app.get('/', (req,res) => res.send('Llegamos al navegadorr!!')) //----> ruta raíz: localhost:3000
+app.listen(port, ()=> console.log("Servidor corriendo en el puerto" + port))
