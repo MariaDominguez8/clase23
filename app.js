8a5debc634d2854b1491600f5efc5d842bd41822
MariaDominguez8 <maria.e.dominguez.97@gmail.com>
2021-05-28 15:58:51 -0300

    agregamos rutas

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ const port= 3000;
 //res -->> response (respuesta)
 
 app.get('/', (req,res) => res.send('Llegamos al navegadorr!!')) //----> ruta raíz: localhost:3000
+
+app.get('/contactos',(req,res) => res.send('Página de contacto')) ;
+app.get('/productos',(req,res) => res.send('Listado de productos')) ;
 app.listen(port, ()=> console.log("Servidor corriendo en el puerto" + port))
